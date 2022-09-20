91af6b75f13fe952e4fc22124cd63e772da30a1c
Selçuk <Seluk@DESKTOP-JP0JKGJ>
2022-09-20 22:18:02 +0300

    Proje dosyası ekle.

---
+++ Process-kill/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Process_kill
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string uygulamaadi;
+            Console.Write("Kapatmak istediğiniz programın adını giriniz: ");
+            uygulamaadi = Console.ReadLine();
+           
+            Process[] programkapat = Process.GetProcessesByName(uygulamaadi);
+            if (programkapat.Length > 0)
+            {
+                foreach (Process p in programkapat)
+                {
+                    p.Kill();
+                }
+                Console.WriteLine("Kapatmak istediğiniz program kapatıldı.");
+            }
+            else
+            {
+                Console.WriteLine("Böyle bir program çalışmamaktadır.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
